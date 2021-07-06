ea0f8ac292dda15b9eb90e2f9a12dd92d18df134
Prathamesh Tibile <prathameshtibile1997@gmail.com>
2021-07-06 20:08:11 +0530

    Main file added using Lambda Expression

---
+++ UserRegistrationLambdaUC13.java
@@ -0,0 +1,41 @@
+/**
+ * @author PRATHAMESH TIBILE
+ * @since 6-7-21
+ * Use Lambda Function to validate First Name, Last Name, Email, Mobile, and Password.
+ */
+package Day23_Lambda;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistrationLambdaUC13  {
+    public static void main(String[] args) {
+        UserRegistrationInterface obj ;
+        obj= ()->{
+            System.out.println("Welcome to User Registration");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter first Name : ");
+            String firstName = scanner.next();
+            System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+
+            System.out.println("Enter last Name : ");
+            String lastName = scanner.next();
+            System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
+
+            System.out.println("Enter email : ");
+            String email = scanner.next();
+            System.out.println(Pattern.matches("^[a-zA-z0-9.+_-]{2,}@[a-zA-z0-9]{1,}[.]{1}[a-z]{2,}$", email));
+
+            System.out.println("Enter phone number: ");
+            String phoneNum = scanner.next();
+            System.out.println(Pattern.matches("^(?:(?:\\\\+|0{0,2})91(\\\\s*[\\\\-]\\\\s*)?|[0]?)?[789]\\\\d{9}$", phoneNum));
+
+            System.out.println("Enter password : ");
+            String password = scanner.next();
+            System.out.println(Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)(?=.*[0-9]).{8,}$", password));
+
+        };
+        obj.userValidate();
+    }
+   
+}
